cce8f2886a05e980ea83a27b69c1f5c3db841089
GeertHuls <geert.huls82@gmail.com>
2013-11-25 22:04:28 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function (grunt) {
+
+require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+grunt.initConfig({
+    jshint: {
+      options: {
+		node: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        require: true,
+        define: true,
+        requirejs: true,
+        describe: true,
+        expect: true,
+        it: true
+      },
+      all: ['Gruntfile.js', 'public/scripts/*.js']
+    },
+
+    // grunt-express will serve the files from the folders listed in `bases`
+    // on specified `port` and `hostname`
+    express: {
+      all: {
+        options: {
+          port: 9000,
+          hostname: "0.0.0.0",
+          bases: ['./public'],
+          livereload: true
+        }
+      }
+    },
+
+    // grunt-watch will monitor the projects files
+    watch: {
+      all: {
+        files: 'index.html',
+        options: {
+          livereload: true
+        }
+      }
+    },
+
+    // grunt-open will open your browser at the project's URL
+    open: {
+      all: {
+        // Gets the port from the connect configuration
+        path: 'http://localhost:<%= express.all.options.port%>'
+      }
+    }
+
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'jshint');
+
+  // Creates the `server` task
+  grunt.registerTask('server', [
+    'express',
+    'open',
+    'watch'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+grunt-livereload
+================
+
+Grunt taks runner + live reload setup.
+
+### Usage
+
+1) npm install
+2) npm install -g grunt (optional)
+3) npm install -g grunt-cli (optional)
+4) grunt server
+
+All changes saved to html, css and js files causes the browser to automatically reload.
